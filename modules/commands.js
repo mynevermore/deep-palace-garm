455be819809c99d9b0ca242e7de821b38aa89f22
Moira <32690275+mynevermore@users.noreply.github.com>
2020-11-24 01:03:45 +0800

    Update commands.js

--- modules/commands.js
+++ modules/commands.js
@@ -81,16 +81,12 @@ class Commands {
         return content;
     }
 
-    async get8Ball(question) {
-        const main = await fetch("https://customapi.aidenwallis.co.uk/api/v1/misc/8ball");
-        const data = await main;
+    get8Ball(question) {
+        const responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."];
+        const data = responses[Math.floor(Math.random() * responses.length)];
         
         console.log(main);
         console.log(data);
-
-        if (!data) {
-        return console.log("Error 01: Unable to access the json content of API");
-        }
 
         let content = {
         embed: {
