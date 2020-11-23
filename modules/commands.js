9149b94f9f9a8ffd23f743a32dcddc56a910d9f9
Moira <32690275+mynevermore@users.noreply.github.com>
2020-11-24 00:45:12 +0800

    Create commands.js

---
+++ modules/commands.js
@@ -0,0 +1,101 @@
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
+
+class Commands {
+    async getMeme() {
+        const main = await fetch("https://no-api-key.com/api/v1/memes");
+        const data = await main.json();
+
+        if (!data) {
+        return console.log("Error 01: Unable to access the json content of API");
+        }
+
+        let content = {
+        embed: {
+            color: "RANDOM",
+            title: "Here's a meme",
+            image: { url: data.image }
+        }
+        };
+
+        return content;
+    }
+
+    async getQuote() {
+        const main = await fetch("https://no-api-key.com/api/v1/memes");
+        const data = await main.json();
+
+        if (!data) {
+        return console.log("Error 01: Unable to access the json content of API");
+        }
+
+        let content = {
+        embed: {
+            color: "RANDOM",
+            title: "Here's a meme",
+            image: { url: data.image }
+        }
+        };
+
+        return content;
+    }
+
+    async getFact() {
+        const main = await fetch("https://no-api-key.com/api/v1/memes");
+        const data = await main.json();
+
+        if (!data) {
+        return console.log("Error 01: Unable to access the json content of API");
+        }
+
+        let content = {
+        embed: {
+            color: "RANDOM",
+            title: "Here's a meme",
+            image: { url: data.image }
+        }
+        };
+
+        return content;
+    }
+
+    async getJoke() {
+        const main = await fetch("https://icanhazdadjoke.com/i");
+        const data = await main.json();
+
+        if (!data) {
+        return console.log("Error 01: Unable to access the json content of API");
+        }
+
+        let content = {
+        embed: {
+            color: "RANDOM",
+            title: "Dad joke time!",
+            description: data.joke
+        }
+        };
+
+        return content;
+    }
+
+    async get8Ball(question) {
+        const main = await fetch("https://customapi.aidenwallis.co.uk/api/v1/misc/8ball");
+        const data = await main.json();
+
+        if (!data) {
+        return console.log("Error 01: Unable to access the json content of API");
+        }
+
+        let content = {
+        embed: {
+            color: "RANDOM",
+            title: "The answer to your question " + question + " is...",
+            description: data;
+        }
+        };
+
+        return content;
+    }
+}
+
+module.exports = Commands;
